c5904bfae6ce51368bb370d76f4e77882618b078
Antonio Toma <antonio.toma@gmail.com>
2014-03-05 14:10:25 +0100

    added scripts for simplified installation of android dependencies

---
+++ install-ot-android-dependencies.cmd
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=android-support-v4.jar -DgroupId=com.google.android -DartifactId=android-support-v4 -Dversion=19.0.1 -Dpackaging=jar
+
+mvn install:install-file -Dfile=android.jar -DgroupId=com.google.android -DartifactId=android -Dversion=19 -Dpackaging=jar
