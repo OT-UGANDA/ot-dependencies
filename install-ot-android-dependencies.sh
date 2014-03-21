1bccb73c4e03fe666b1cf760bb1d285cf5c13d57
Antonio Toma <antonio.toma@gmail.com>
2014-03-21 13:10:44 +0100

    Removed osmdroid dependencies and added Google Maps and Play services

--- install-ot-android-dependencies.sh
+++ install-ot-android-dependencies.sh
@@ -1,3 +1,7 @@
 mvn install:install-file -Dfile=android-support-v4.jar -DgroupId=com.google.android -DartifactId=android-support-v4 -Dversion=19.0.1 -Dpackaging=jar
 
 mvn install:install-file -Dfile=android.jar -DgroupId=com.google.android -DartifactId=android -Dversion=19 -Dpackaging=jar
+
+mvn install:install-file -Dfile=google-play-services.apklib -DgroupId=com.google.android.gms -DartifactId=google-play-services -Dversion=4.2.42 -Dpackaging=apklib
+
+mvn install:install-file -Dfile=maps.jar -DgroupId=com.google.android -DartifactId=maps -Dversion=19-1 -Dpackaging=jar
