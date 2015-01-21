e51a4e199422d2778b75e27475c3a262994d0443
Antonio Toma <antonio.toma@gmail.com>
2015-01-21 22:16:00 +0100

    Updated android.jar to API 21

--- install-ot-android-dependencies.cmd
+++ install-ot-android-dependencies.cmd
@@ -1,6 +1,6 @@
 mvn install:install-file -Dfile=android-support-v4.jar -DgroupId=com.google.android -DartifactId=android-support-v4 -Dversion=19.0.1 -Dpackaging=jar
 
-mvn install:install-file -Dfile=android.jar -DgroupId=com.google.android -DartifactId=android -Dversion=19 -Dpackaging=jar
+mvn install:install-file -Dfile=android.jar -DgroupId=com.google.android -DartifactId=android -Dversion=21 -Dpackaging=jar
 
 mvn install:install-file -Dfile=google-play-services.apklib -DgroupId=com.google.android.gms -DartifactId=google-play-services -Dversion=4.2.42 -Dpackaging=apklib
 
